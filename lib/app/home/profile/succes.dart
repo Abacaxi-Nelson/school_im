2a3cb363e3bd703f766e7bd04ad0aa032521c36b
nelson nunes <nelsonnunes@nelsons-iMac.local>
2020-10-26 21:13:16 +0100

    dfgdfgdfg

--- lib/app/home/profile/succes.dart
+++ lib/app/home/profile/succes.dart
@@ -8,8 +8,14 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:school_im/routing/app_router.dart';
 
 class SuccesPage extends ConsumerWidget {
+  AnimationController animationController;
+
   @override
   Widget build(BuildContext context, ScopedReader watch) {
+    final database = context.read(databaseProvider);
+    final firebaseAuth = context.read(firebaseAuthProvider);
+    final user = firebaseAuth.currentUser;
+
     return Scaffold(
         backgroundColor: const Color(0xff9188E5),
         body: Padding(
@@ -28,10 +34,7 @@ class SuccesPage extends ConsumerWidget {
                     style:
                         TextStyle(fontWeight: FontWeight.normal, fontSize: 17.0, color: Colors.white.withOpacity(0.7))),
                 const SizedBox(height: 30.0),
-                Container(
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle, border: Border.all(color: Colors.white.withOpacity(0.4), width: 10)),
-                    child: Icon(Icons.offline_bolt, size: 160, color: Color(0xffFFCA5D).withOpacity(0.8))),
+                IconAnimation(),
                 const SizedBox(height: 30.0),
                 CustomButton(
                   borderColor: Colors.transparent,
@@ -42,7 +45,76 @@ class SuccesPage extends ConsumerWidget {
                     print("press");
                   },
                 ),
+                const SizedBox(height: 10.0),
+                CustomButton(
+                  borderColor: Colors.transparent,
+                  color: Colors.transparent,
+                  color2: const Color(0xff0D0A06),
+                  label: 'Hey c\'est ok, mes parents ont valide !',
+                  onPressed: () async {
+                    print("press ok");
+                    Profile profile = await database.getProfile(user.uid);
+                    if (profile.isValide()) {
+                      await Navigator.of(context, rootNavigator: true).pushNamed(AppRoutes.dashboardPage);
+                    } else {
+                      final snackBar = SnackBar(content: Text('Toujours en attente de validation'));
+                      Scaffold.of(context).showSnackBar(snackBar);
+                    }
+                  },
+                ),
               ]),
         ));
+  }
+}
+
+class IconAnimation extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _IconAnimationState();
+}
+
+class _IconAnimationState extends State<IconAnimation> with TickerProviderStateMixin {
+  AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    )
+      ..forward()
+      ..repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: animationController,
+        builder: (context, child) {
+          return Container(
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.white.withOpacity(animationController.value), width: 10)),
+              child: Icon(Icons.offline_bolt, size: 160, color: Color(0xffFFCA5D).withOpacity(0.8)));
+          /*
+          Container(
+            decoration: ShapeDecoration(
+              color: Colors.white.withOpacity(0.5),
+              shape: CircleBorder(),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(8.0 * animationController.value),
+              child: child,
+            ),
+          );
+          */
+        });
   }
 }
